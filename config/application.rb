9701c3ccff14fb3daa66ab8c547bedf5409a0d0e
pinotnoir72 <neldah72@gmail.com>
2019-10-18 04:09:23 +0000

    fix

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Bundler.require(:default, :assets, Rails.env)
-# Enable the asset pipeline
-config.assets.enabled = true
-# Version of your assets, change this if you want to expire all your assets
-config.assets.version = '1.0'
+
 module Workspace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
