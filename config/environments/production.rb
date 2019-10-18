7dd18fbd2b24431b071d8da1c2e351e968c35efd
pinotnoir72 <neldah72@gmail.com>
2019-10-18 04:06:25 +0000

    config assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     }
 
   #Heroku fix for asset pipeline
-    config.assets.compile = true
+    config.assets.compile = false
     config.assets.digest = true
     
  # ActionCable setup for Heroku
